ea64ff053cc5e9346d137e0f21c7ea6d00891983
Bogdan <tischenkob@gmail.com>
2020-07-21 11:49:15 +0300

    Added set_body

--- abstract/StateMachine.gd
+++ abstract/StateMachine.gd
@@ -5,7 +5,7 @@ signal state_changed
 
 export(NodePath) var _initial_state: NodePath
 
-onready var _body = get_parent()
+onready var _body = get_parent() setget set_body
 
 var _state = null setget set_state
 
@@ -20,8 +20,11 @@ func set_state(state):
 	if _state:
 		_state.exit()
 	self._state = state
-	self._state._body = _body
 	_state.enter()
+
+func set_body(body):
+	self._body = body
+	self._state._body = _body
 
 func add_state(state):
 	add_child(state)
