0e7527d81d6f5ed7b328121ed1654747ee8e3190
Bogdan <tischenkob@gmail.com>
2020-07-21 10:35:01 +0300

    changed body type and enter functions

--- abstract/State.gd
+++ abstract/State.gd
@@ -1,14 +1,14 @@
 extends Node
 class_name State
 
-var body : PhysicsBody2D = null
+var body : Node = null
 
 func handle_input(event):
 	for child in get_children():
 		if child is Action:
 			child.handle_input(event)
 
-func enter(body) -> void:
+func enter() -> void:
 	self.body = body
 
 func exit() -> void:
